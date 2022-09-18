187f010b647d772f9be1dafce198dbf158a8ee39
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-09-17 20:49:43 -0400

    Switch to src/index.js as main entry point

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//49. We'll change the default entry point of our server, from src/server.js to use a new file: src/index.js. 
+//In this file, we'll begin by loading environment variables from an .env file as the first thing we do.
+
+// Read environment variables from an .env file (if present)
+require('dotenv').config();
+
+// We want to log any crash cases so we can debug later from logs.
+const logger = require('./logger');
+
+// If we're going to crash because of an uncaught exception, log it first.
+// https://nodejs.org/api/process.html#event-uncaughtexception
+process.on('uncaughtException', (err, origin) => {
+  logger.fatal({ err, origin }, 'uncaughtException');
+  throw err;
+});
+
+// If we're going to crash because of an unhandled promise rejection, log it first.
+// https://nodejs.org/api/process.html#event-unhandledrejection
+process.on('unhandledRejection', (reason, promise) => {
+  logger.fatal({ reason, promise }, 'unhandledRejection');
+  throw reason;
+});
+
+// Start our server
+require('./server');
