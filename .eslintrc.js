73fb98f265caac1ec73ef051305768fabbca328a
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-09-20 21:54:48 -0400

    Add jest files and changed configuration in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "commonjs": true,
-        "es2021": true
+    env: {
+      node: true,
+      commonjs: true,
+      es2021: true,
+      // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+      jest: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    extends: "eslint:recommended",
+    parserOptions: {
+      ecmaVersion: 13
     },
-    "rules": {
+    rules: {
     }
 }
