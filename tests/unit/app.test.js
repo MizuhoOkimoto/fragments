07817d1032beb83825a0fe9dffa7302339111efd
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-09-21 19:47:37 -0400

    Improve test coverage for app.js

---
+++ tests/unit/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('404 middleware test', () => {
+  test('should return HTTP 404 not found', async () => {
+    const res = await request(app).get('/error');
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe('error');
+    expect(res.body.error.code).toBe(404);
+    expect(res.body.error.message).toBe('not found');
+  })
+});
