3b0b0a436c5121dfa6b745c676cdd9f6cc281b9d
Mizuho Okimoto <mokimoto@myseneca.ca>
2022-11-27 15:09:23 -0500

    Modify the unit test to pass the ci and add more comments

--- tests/unit/delete.test.js
+++ tests/unit/delete.test.js
@@ -5,10 +5,11 @@ describe('DELETE /fragments/:id', () => {
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
 
-  // // If the wrong username/password pair are used (no such user), it should be forbidden
+  // If the wrong username/password pair are used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
+  // Authenticated users with correct content-type can delete the existing fragment
   test('authenticated users can delete a fragment', async () => {
     const postRes = await request(app)
       .post('/v1/fragments')
@@ -23,6 +24,7 @@ describe('DELETE /fragments/:id', () => {
     expect(deleteRes.body.status).toBe('ok');
   });
 
+  // if the fragment is not found by id, it return 404 Not Found
   test('If the id is not found, returns an HTTP 404 with an appropriate error message', async () => {
     const deleteRes = await request(app)
       .delete(`/v1/fragments/notexistid`)
--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -1,4 +1,3 @@
-
 const request = require('supertest');
 const app = require('../../src/app');
 //const { readFragment } = require('../../src/model/data');
@@ -20,108 +19,97 @@ describe('GET /v1/fragments', () => {
   });
 
   // When fragments can't get byUser function; Returns 404 status
-  test('invalid id returns 404 status code ', async () => {
-    const res = await request(app)
-    .get('/v1/fragments')
-    .auth('user1@email.com', 'password111');
+  test('invalid id returns 401 status code ', async () => {
+    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password111');
     expect(res.statusCode).toBe(401);
   });
-
 });
 
-  describe('GET /fragments/:id', () => {
-    // No auth returns unauthrized 401 status code
-    test('unauthenticated requests should returns error', async () =>
-      await request(app).get('/v1/fragments/id').expect(401));
+describe('GET /fragments/:id', () => {
+  // No auth returns unauthrized 401 status code
+  test('unauthenticated requests should returns error', async () =>
+    await request(app).get('/v1/fragments/id').expect(401));
 
-    // Wrong username and password returns unauthrized 401
-    test('incorrect credentials are denied to access; returns unauthrized 401 ', async() =>
-       await request(app)
-        .get('/v1/fragments/id')
-        .auth('invalid@email.com', 'incorrect_password')
-        .expect(401));
+  // Wrong username and password returns unauthrized 401
+  test('incorrect credentials are denied to access; returns unauthrized 401 ', async () =>
+    await request(app)
+      .get('/v1/fragments/id')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
 
-    // Using a valid id returns 200 status
-    test('valid id returns 200', async () => {
-      const res = await request(app)
-      .get('/v1/fragments')
-      .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(200);
-    });
-
-    // Using an invalid id returns 400 status
-    test('invalid id returns 400', async () => {
-      const res = await request(app)
-        .get('/v1/fragments/invalid')
-        .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(400);
-    });
+  // Using a valid id returns 200 status
+  test('valid id returns 200', async () => {
+    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
   });
 
-  describe('GET /fragments/:id/info', () => {
-    // When text fragment is created and returned; request succeeded 200 (201 is created)
-    test('Info: returns an existing fragments metadata', async () => {
-      const createRes = await request(app)
+  // Using an invalid id returns 400 status
+  test('invalid id returns 404', async () => {
+    const res = await request(app)
+      .get('/v1/fragments/invalid')
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe('GET /fragments/:id/info', () => {
+  // When text fragment is created and returned; request succeeded 200 (201 is created)
+  test('Info: returns an existing fragments metadata', async () => {
+    const createRes = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
       .send({
         body: 'text fragment',
-        type: 'text/plain'
+        type: 'text/plain',
       });
-      const id = await createRes.body.fragment.id;
-      const res = await request(app)
-        .get(`/v1/fragments/${id}/info`)
-        .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(200);
-      expect(res.body.status).toBe('ok');
-    });
-
-    // Invalid info returns 404 status
-    test('Info: invalid id returns 404 status code for info', async () => {
-      const res = await request(app)
-      .get('/v1/fragments/invalid/info')
+    const id = await createRes.body.fragment.id;
+    const res = await request(app)
+      .get(`/v1/fragments/${id}/info`)
       .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(400);
-      expect(res.body.status).toBe('error');
-    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe('ok');
   });
 
-  describe('GET /fragments/:id.ext', () => {
-    // Returns an existing fragment's data converted to a supported type
-    test('.ext: Support Markdown fragments (.md) converted to HTML (.html)', async () => {
-      const createRes = await request(app)
+  // Invalid info returns 404 status
+  test('Info: invalid id returns 404 status code for info', async () => {
+    const res = await request(app)
+      .get('/v1/fragments/invalid/info')
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(400);
+    expect(res.body.status).toBe('error');
+  });
+});
+
+describe('GET /fragments/:id.ext', () => {
+  // Returns an existing fragment's data converted to a supported type
+  test('.ext: Support Markdown fragments (.md) converted to HTML (.html)', async () => {
+    const createRes = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
       .send({
         body: 'text fragment',
         //type: 'text/plain'
       });
-      const id = await createRes.body.fragment.id;
-      const res = await request(app)
-        .get(`/v1/fragments/${id}.html`)
-        .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(200);
-    });
+    const id = await createRes.body.fragment.id;
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.html`)
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
+  });
 
-    // Returns an existing fragment's data converted to a supported type
-    test('.ext: check if .txt is supported type', async () => {
-      const createRes = await request(app)
+  // Returns an existing fragment's data converted to a supported type
+  test('.ext: check if .txt is supported type', async () => {
+    const createRes = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
       .send({
         body: 'text fragment',
-        type: 'text/plain'
+        type: 'text/plain',
       });
-      const id = await createRes.body.fragment.id;
-      const res = await request(app)
-        .get(`/v1/fragments/${id}.txt`)
-        .auth('user1@email.com', 'password1');
-      expect(res.statusCode).toBe(200);
-    });
-
+    const id = await createRes.body.fragment.id;
+    const res = await request(app)
+      .get(`/v1/fragments/${id}.txt`)
+      .auth('user1@email.com', 'password1');
+    expect(res.statusCode).toBe(200);
   });
-
-
-
-
-
+});
